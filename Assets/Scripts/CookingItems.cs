985c3736fc2d4ad9ac7dcad6425e1b81aa79a38e
hassy1 <48172849+hassy1@users.noreply.github.com>
2026-07-12 21:01:25 +0500

    egg animation updated

--- Assets/Scripts/CookingItems.cs
+++ Assets/Scripts/CookingItems.cs
@@ -4,11 +4,16 @@ namespace RestaurantTycoon
 {
     public class CookingItems : MonoBehaviour
     {
+        public float animationDelay = 0;
         public Animator anim;
         public GameObject startCooking;
         public void StartCooking()
         {
             startCooking.SetActive(true);
+            Invoke("DelayAnimation", animationDelay);
+        }
+        void DelayAnimation()
+        {
             anim.SetTrigger("Cook");
         }
         public void EndCooking()
